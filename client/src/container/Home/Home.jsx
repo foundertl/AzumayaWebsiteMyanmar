fd7fbba4e0fdb6ddc9fa75a0d1d9bf62654db407
foundertl <nguyenductunglam@gmail.com>
2024-04-10 14:30:06 +0700

    up code

--- client/src/container/Home/Home.jsx
+++ client/src/container/Home/Home.jsx
@@ -10,7 +10,6 @@ import withAutoplay from 'react-awesome-slider/dist/autoplay';
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 import HelmetLayout from "../../components/HelmetLayout/HelmetLayout";
-import TabsInput from "../../Test";
 
 function LineModal(props) {
   return (
@@ -44,7 +43,7 @@ function LineModal(props) {
 
 export default function Home({news}) {
   const { t } = useTranslation();
-  const featureItem = t("feature.feature_item", { returnObjects: true });
+  const featureItem = t("feature_pp.feature_item", { returnObjects: true });
   const homeNews = news.slice(0,4)
   const caption = t("caption", {returnObjects: true})
   const [isOpen, setIsOpen] = useState(false)
@@ -80,56 +79,6 @@ const AutoPlaySlider =  withAutoplay(AwesomeSlider)
                           ))}
         </AutoPlaySlider>
       </div>
-      <div className="content">
-          <div className="container-fluid">
-            <div className="row g-0 p-0">
-              <div className="col-6 col-md-3 offset-0">
-                <div className="content__branch-item">
-                  <Link className='link-route' to = '/hotel-hn' >
-                  <img
-                    className="content__branch-img"
-                    src="https://res.cloudinary.com/dtdfsaaei/image/upload/v1710735800/AzumayaWeb/hanoi1_yrqgvy.png"
-                    alt="hotel hanoi azumaya hotel"
-                  />
-                  </Link>
-                </div>
-              </div>
-              <div className="col-6 col-md-3 offset-0">
-                <div className="content__branch-item">
-                <Link className='link-route' to = '/hotel-hcm' >
-                  <img
-                    className="content__branch-img"
-                    src="https://res.cloudinary.com/dtdfsaaei/image/upload/v1710735801/AzumayaWeb/hochiminh1_glyoyn.png"
-                    alt="hotel ho chi minh azumaya hotel"
-                  />
-                 </Link>
-                </div>
-              </div>
-              <div className="col-6 col-md-3 offset-0">
-                <div className="content__branch-item">
-                <Link className='link-route' to = '/hotel-dn' >
-                  <img
-                    className="content__branch-img"
-                    src="https://res.cloudinary.com/dtdfsaaei/image/upload/v1710735801/AzumayaWeb/danang1_om37cr.png"
-                    alt="hotel da nang azumaya hotel"
-                  />
-                 </Link>
-                </div>
-              </div>
-              <div className="col-6 col-md-3 offset-0">
-                <div className="content__branch-item">
-                <Link className='link-route' to = '/hotel-hp' >
-                  <img
-                    className="content__branch-img"
-                    src="https://res.cloudinary.com/dtdfsaaei/image/upload/v1710735806/AzumayaWeb/haiphong1_vr2jyt.png"
-                    alt="hotel hai phong azumaya"
-                  />
-                 </Link>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
       <div className="is-sticky">
         <Booking />
       </div>
@@ -141,182 +90,40 @@ const AutoPlaySlider =  withAutoplay(AwesomeSlider)
                 <h2 className="content__news-title" style={{fontWeight:'bold'}}>{t("home.news_title")}</h2>
                 <ul className="content__news-list">
                 {homeNews.map((article) => {
-               const parsedDate = parse(article.date, 'yyyy-MM-dd', new Date()); 
-               const formattedDate = format(parsedDate, 'MMM do yyyy')
-               const [all, month, day, suffix, year] = formattedDate.match(/(\w+) (\d+)(\w+) (\d+)/);
-               console.log(formattedDate);
-              if (article.allBranch == true)
-               {
-                return (
-                  <div className="row justify-content-evenly">
-                    <div className="col-md-2 mt-1 news_date-container">
-                      <div className="news_box1">
-                        <div className="news_time-home">
-                        <div className="month">{month}</div>
-                        <div className="day pl-2">{day}</div>
-                        <sub className="suffix pt-2">{suffix}</sub>
-                        <div className="year pl-2">{year}</div>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="col-md-2 news_branch-container">
-                          <div className="allbranch allbranch_home">{t("header.vietnam")}</div>
-                    </div>
-                    <div className="col-md-7 mt-2 news_title-container">
-                      <div >
-                        <Link
-                        key={article.id}
-                          className="news_title news_homeTitle"
-                          to={`/News/${encodeURIComponent(article.title)}`}
-                        >
-                          <div className="article_title">{article.title}</div>
-                        </Link>
-                      </div>
-                    </div>
-                  </div>
-                );
-              } else if (article.hn == true) {
-                return (
-                  <div className="row justify-content-evenly">
-                    <div className="col-md-2 mt-1 news_date-container" >
-                      <div className="news_box1">
-                        <div className="news_time-home">
-                        <div className="month">{month}</div>
-                        <div className="day pl-2">{day}</div>
-                        <sub className="suffix pt-2">{suffix}</sub>
-                        <div className="year pl-2">{year}</div>
-                        </div>
-                      </div>
-                    </div>
-                        <div className="col-md-2 news_branch-container" >
-                          <div className="hanoi hanoi_home">{t("header.hn")}</div>
-                        </div>
-                    <div className="col-md-7 mt-2 news_title-container">
-                      <div key={article.id}>
-                        <Link
-                          className="news_title news_homeTitle"
-                          to={`/News/${encodeURIComponent(article.title)}`}
-                        >
-                          <div className="article_title">{article.title}</div>
-                        </Link>
-                      </div>
-                    </div>
-                  </div>
-                );
-              } else if (article.hp == true) {
-                return (
-                  <div className="row justify-content-evenly">
-                    <div className="col-md-2 mt-1 news_date-container" >
-                      <div className="news_box1">
-                        <div className="news_time-home">
-                        <div className="month">{month}</div>
-                        <div className="day pl-2">{day}</div>
-                        <sub className="suffix pt-2">{suffix}</sub>
-                        <div className="year pl-2">{year}</div>
-                        </div>
-                      </div>
-                    </div>
-                        <div className="col-md-2 news_branch-container" >
-                          <div className="haiphong haiphong_home">{t("header.hp")}</div>
-                        </div>
-                    <div className="col-md-7 mt-2 news_title-container">
-                      <div key={article.id}>
-                        <Link
-                          className="news_title"
-                          to={`/News/${encodeURIComponent(article.title)}`}
-                        >
-                          <div className="article_title">{article.title}</div>
-                        </Link>
-                      </div>
-                    </div>
-                  </div>
-                );
-              } else if (article.dn == true) {
-                return (
-                  <div className="row justify-content-evenly">
-                    <div className="col-md-2 mt-1 news_date-container" >
-                      <div className="news_box1">
-                        <div className="news_time-home">
-                          <div className="month">{month}</div>
-                        <div className="day pl-2">{day}</div>
-                        <sub className="suffix pt-2">{suffix}</sub>
-                        <div className="year pl-2">{year}</div>
-                        </div>
-                      </div>
-                    </div>
-                        <div className="col-md-2 news_branch-container" >
-                          <div className="danang danang_home">{t("header.dn")}</div>
-                        </div>
-                    <div className="col-md-7 mt-2 news_title-container"> 
-                      <div key={article.id}>
-                        <Link
-                          className="news_title"
-                          to={`/News/${encodeURIComponent(article.title)}`}
-                        >
-                          <div className="article_title">{article.title}</div>
-                        </Link>
-                      </div>
-                    </div>
-                  </div>
-                );
-              } else if (article.hcm == true) {
-                return (
-                  <div className="row justify-content-evenly">
-                    <div className="col-md-2 mt-1 news_date-container" >
-                      <div className="news_box1">
-                        <div className="news_time-home">
-                          <div className="month">{month}</div>
-                        <div className="day pl-2">{day}</div>
-                        <sub className="suffix pt-2">{suffix}</sub>
-                        <div className="year pl-2">{year}</div>
-                        </div>
-                      </div>
-                    </div>
-                        <div className="col-md-2 news_branch-container" >
-                          <div className="hochiminh hochiminh_home">{t("header.hcm")}</div>
-                        </div>
-                    <div className="col-md-7 mt-2 news_title-container">
-                      <div key={article.id}>
-                        <Link
-                          className="news_title news_homeTitle"
-                          to={`/News/${encodeURIComponent(article.title)}`}
-                        >
-                          <div className="article_title">{article.title}</div>
-                        </Link>
-                      </div>
-                    </div>
-                  </div>
-                );
-              } else if (article.az == true) {
-                return (
-                  <div className="row justify-content-evenly">
-                    <div className="col-md-2 mt-1 news_date-container" >
-                      <div className="news_box1">
-                        <div className="news_time-home">
-                          <div className="month">{month}</div>
-                        <div className="day pl-2">{day}</div>
-                        <sub className="suffix pt-2">{suffix}</sub>
-                        <div className="year pl-2">{year}</div>
-                        </div>
-                      </div>
-                    </div>
-                        <div className="col-md-2 news_branch-container" >
-                          <div className="azumaya azumaya_home">{t("header.az")}</div>
-                        </div>
-                    <div className="col-md-7 mt-2 news_title-container">
-                      <div key={article.id}>
-                        <Link
-                          className="news_title news_homeTitle"
-                          to={`/News/${encodeURIComponent(article.title)}`}
-                        >
-                          <div className="article_title">{article.title}</div>
-                        </Link>
-                      </div>
-                    </div>
-                    </div>
-                );
-              }
-            })}
+                 const parsedDate = parse(article.date, 'yyyy-MM-dd', new Date()); 
+                 const formattedDate = format(parsedDate, 'MMM do yyyy')
+                 const [all, month, day, suffix, year] = formattedDate.match(/(\w+) (\d+)(\w+) (\d+)/);
+                 console.log(formattedDate);
+                 return(
+                   <div className="row">
+                       <div className="col-md-2 mt-1 news_date-container">
+                         <div className="news_box1">
+                           <div className="news_time-home">
+                           <div className="month">{month}</div>
+                           <div className="day pl-2">{day}</div>
+                           <sub className="suffix pt-2">{suffix}</sub>
+                           <div className="year pl-2">{year}</div>
+                           </div>
+                         </div>
+                       </div>
+                       <div className="col-md-2 news_branch-container">
+                             <div className="phnompenh phnompenh_home">{t("header.pp")}</div>
+                       </div>
+                       <div className="col-md-7 mt-2 news_title-container">
+                         <div key={article.id}>
+                           <Link
+                             className="news_title news_homeTitle"
+                             to={`/News/${encodeURIComponent(article.title)}`}
+                           >
+                             <div className="article_title">{article.title}</div>
+                           </Link>
+                         </div>
+                       </div>
+                     </div>
+                          
+                   );
+                 }
+               )}
                 </ul>
               </div>
                 <div className="col-md-2">
@@ -347,15 +154,14 @@ const AutoPlaySlider =  withAutoplay(AwesomeSlider)
             <div className="row align-item-center">
               <div className="col-md-12 p-3">
                 <h1 className="content__welcome-text">
-                  {t("home.welcome_title")}
-                  <small className="welcome_text-small">{t("home.welcome_sub-title")}</small>
+                  {t("home_pp.welcome_title")}
+                  <small className="welcome_text-small">{t("home_pp.welcome_sub-title")}</small>
                 </h1>
-                <p className="welcome-content">{t("home.welcome_content")}</p>
+                <p className="welcome-content">{t("home_pp.welcome_content")}</p>
               </div>
             </div>
           </div>
         </div>
-        {/* <TabsInput /> */}
       </AnimatedOnScroll>
       <AnimatedOnScroll>
         <div className="content__feature">
